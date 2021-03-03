d11b709340f93b18e83441451840a8761fc43d15
JadenCho <34437519+JadenCho@users.noreply.github.com>
2021-03-02 20:02:11 -0700

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,4 +1,6 @@
-def calc():
+def calc(F, V):
+
+	import math
 
 	betafile = open("C:\\cygwin_64\\home\\JadenCho\\ardupilot\\build\\sitl\\bin\\buffer.bin", "r")
 
@@ -9,22 +11,44 @@ def calc():
 
 	beta = float(b)
 
-	gspeedstrl = d[0]
-	Yvelstrl = d[1]
+	if d == [] or b == []:
+		Ft = F
+		Vt = V
+	else:
 
-	gspeedstr = gspeedstrl.split(', ')
-	Yvelstr = Yvelstrl.split(', ')
+		gspeedstrl = d[0]
+		Yvelstrl = d[1]
+		force_upstrl = d[3]
 
-	gspeed = gspeedstr[1].split('\n')
-	Yvel = Yvelstr[1].split('\n')
+		gspeedstr = gspeedstrl.split(', ')
+		Yvelstr = Yvelstrl.split(', ')
+		force_upstr = force_upstrl.split(', ')
 
-	truegspeed = float(gspeed[0])
-	trueYvel = float(Yvel[0])
+		gspeed = gspeedstr[1].split('\n')
+		Yvel = Yvelstr[1].split('\n')
+		force_up = force_upstr[1].split('\n')
 
-	print(truegspeed)
-	print(trueYvel)
+		truegspeed = float(gspeed[0])
+		trueYvel = float(Yvel[0])
+		trueforce_up = float(force_up[0])
 
-	xlist = [truegspeed, trueYvel, beta]
+		#print(truegspeed)
+		#print(trueYvel)
+		#print(trueforce_up)
+		#print(beta)
+
+		Ft = trueforce_up/math.sin(beta) #Units of N = Kg/ms^2
+
+		Vt = truegspeed/math.cos(beta) #Units of m/s
+
+	#print(Ft)
+	#print(Vt)
+
+	power = abs(Ft)*abs(Vt) #Units of Watts
+
+	xlist = [power, Ft, Vt]
 
 	return xlist
 
+#if __name__ == "__main__":
+#	main()
