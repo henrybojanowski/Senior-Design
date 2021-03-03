eb7e896cc200a7fb020fe123575e001927b434d2
JadenCho <34437519+JadenCho@users.noreply.github.com>
2021-03-02 20:02:48 -0700

    Update mainx.py

--- mainx.py
+++ mainx.py
@@ -1,13 +1,17 @@
 from calculate import *
 from plot import *
-def main(x_vec, y_vec,line1):
+def main(x_vec, y_vec,line1, F, V):
 
-	power = calc()
+	xlist = calc(F, V)
 	line1 = live_plotter(x_vec, y_vec, line1)
 
 	#print(xlist)
 
-	plist = [line1, power]
+	plist = [line1, xlist]
+
+	print("Power is: ", xlist[0])
+	print("Force is: ", xlist[1])
+	print("Velocity is: ", xlist[2])
 
 	return plist
 
@@ -18,9 +22,9 @@ if __name__ == "__main__":
 	y_vec = np.zeros(len(x_vec))
 	#print(y_vec)
 	line1 = []
-	plist = main(x_vec, y_vec, line1)
+	plist = main(x_vec, y_vec, line1, 0, 0)
 	
 	while True:
-		y_vec[-1] = plist[1]
-		plist = main(x_vec, y_vec, plist[0])
+		y_vec[-1] = plist[1][0]
+		plist = main(x_vec, y_vec, plist[0], plist[1][1], plist[1][2])
 		y_vec = np.append(y_vec[1:],0.0)
